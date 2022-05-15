52ec8aab38a34e2e17683fa8e0e9cc2733c60a47
Arhipov Evgeniy <arhipovea@gmail.com>
2022-05-15 15:24:19 +0300

    vector add

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "terraform-registry.storage.yandexcloud.net/yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = var.yc_oauth
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = var.yc_zone
+}
+
+#========== Templates ===========================
+resource "local_file" "hosts_cfg" {
+  content = templatefile("${path.module}/templates/hosts.tpl",
+    {
+      clickhouse_01_ext_ip = yandex_compute_instance.clickhouse_01.network_interface.0.nat_ip_address      
+      vector_01_ext_ip = yandex_compute_instance.vector_01.network_interface.0.nat_ip_address      
+    }
+  )
+  filename = "../ansible/playbook/inventory/prod.yml"
+}
---
+++ terraform/templates/hosts.tpl
@@ -0,0 +1,13 @@
+---
+clickhouse:
+  hosts:
+    clickhouse-01:
+      ansible_host: ${clickhouse_01_ext_ip}
+      ansible_user: ansible
+      allow_world_readable_tmpfiles: true
+vector:
+  hosts:
+    vector-01:
+      ansible_host: ${vector_01_ext_ip}
+      ansible_user: ansible
+      allow_world_readable_tmpfiles: true
