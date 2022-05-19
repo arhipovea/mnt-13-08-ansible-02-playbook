1a741f073620711969767ab34757dc074e536f9c
Evgeniy Arkhipov <arhipovea@gmail.com>
2022-05-19 13:58:40 +0300

    Fix vector

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,15 @@ resource "local_file" "hosts_cfg" {
   )
   filename = "../ansible/playbook/inventory/prod.yml"
 }
+
+resource "local_file" "ip_cfg" {
+  content = templatefile("${path.module}/templates/ip.tpl",
+    {
+      clickhouse_01_ext_ip = yandex_compute_instance.clickhouse_01.network_interface.0.nat_ip_address      
+      clickhouse_01_int_ip = yandex_compute_instance.clickhouse_01.network_interface.0.ip_address      
+      vector_01_ext_ip = yandex_compute_instance.vector_01.network_interface.0.nat_ip_address      
+      vector_01_int_ip = yandex_compute_instance.vector_01.network_interface.0.ip_address      
+    }
+  )
+  filename = "../ansible/playbook/group_vars/all.yml"
+}
---
+++ terraform/templates/ip.tpl
@@ -0,0 +1,5 @@
+---
+clickhouse_01_ext_ip: ${clickhouse_01_ext_ip}
+clickhouse_01_int_ip: ${clickhouse_01_int_ip}
+vector_01_ext_ip: ${vector_01_ext_ip}
+vector_01_int_ip: ${vector_01_int_ip}
